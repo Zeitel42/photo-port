1d7f5a9a5b7fe77cbe9fc993a904f47dfda0e5d1
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-06-26 08:15:53 -0400

    fixed Modal bug, added Modal testing, ALL tests pass

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React, { useState } from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Gallery from "./components/Gallery";
-import ContactForm from "./components/Contact";
+import ContactForm from "./components/ContactForm";
 
 function App() {
-  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: "commercial",
@@ -21,6 +20,8 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
